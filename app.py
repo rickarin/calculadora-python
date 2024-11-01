89f9d771bc25f76bc5389476646ced538e647230
rickarin <ricardofelipecrvg@gmail.com>
2024-11-01 15:32:24 -0300

    Programa se encerra se não for escolhido uma Ação de Adição, Subtração, Multiplicação ou Divisão

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
+import sys
+
 print('Você ira digitar seu numero, logo em seguida podera escolher entre 4 Operações, Digite / para divisão, Digite * para multiplicação, Digite + para adição e Digite - para subtração')
 
 
 acao = input('Escolha a operação: ')
 
+if acao not in ["/", "*", "+", "-"]:
+    print('Operação inválida! O programa será encerrado.')
+    sys.exit() 
+
 num1 = int(input('Digite um numero: '))
 num2 = int(input('Digite um numero: '))
 total = 0
 
-
-if acao == "/" :
-    total = num1 / num2
-    print(total)
-elif acao == '*' :
+if acao == "/":
+    if num2 != 0:  # Verifica se num2 não é zero
+        total = num1 / num2
+        print("Resultado:", total)
+    else:
+        print("Erro: Divisão por zero não é permitida.")
+elif acao == '*':
     total = num1 * num2
-    print(total)
-elif acao == '+' :
+    print("Resultado:", total)
+elif acao == '+':
     total = num1 + num2
-    print(total)
-elif acao == '-' :
+    print("Resultado:", total)
+elif acao == '-':
     total = num1 - num2
-    print(total)
+    print("Resultado:", total)
 else:
-    print('Operação Invalida')
+    print('Operação inválida!')
 
